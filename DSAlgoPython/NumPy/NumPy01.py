3e68c8325053e226c54ef2435ca0ccf5a893ebe0
apple <satyapriya.arch@gmail.com>
2017-12-26 16:35:46 +0530

    Commit

---
+++ DSAlgoPython/NumPy/NumPy01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 16:30:29 2017
+
+@author: apple
+"""
+
+import numpy as np
+
+array = np.arange(100)
+
+
+for item in array:
+    print(item)
