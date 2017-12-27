f1b34e74224bc9318ac5ff602afb8a06e9c0213e
apple <satyapriya.arch@gmail.com>
2017-12-27 19:16:19 +0530

    55

---
+++ DSAlgoPython/NumPy/HelloTensorFlow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 19:00:15 2017
+
+@author: apple
+"""
+
+# Python
+import tensorflow as tf
+
+node1 = tf.constant([], dtype=tf.float32 , shape=(2,3))
+
+
+print(node1)
+
---
+++ DSAlgoPython/NumPy/tensorflowtest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 18:04:52 2017
+
+@author: apple
+"""
+
+import tensorflow as tf
+
+
+# Creates a graph.
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
